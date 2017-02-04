02574491b6d6a3226fe134ee7031851b9105c846
Sandor Drieënhuizen <sandor@subspace.nl>
2017-02-04 17:57:25 +0100

    Added readme/license files and updated about window
    - Added the readme.txt file with a reference to the GitHub page.
    - Added a LICENSE file.
    - Added a link to the about window, referring to the GitHub page.
    - Added a button to the about window that shows the license details.

--- src/DpiGonzales/MainWindow.xaml.cs
+++ src/DpiGonzales/MainWindow.xaml.cs
@@ -1,9 +1,15 @@
-﻿using System.Windows;
+﻿using NLog;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
 
 namespace DpiGonzales
 {
     public partial class MainWindow : Window
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -14,6 +20,32 @@ namespace DpiGonzales
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void SupportWebsiteHyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
+
+        private void LicenseButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var tempFileName = Path.GetTempFileName();
+                var licenseTextFile = Path.ChangeExtension(tempFileName, ".txt");
+                File.Move(tempFileName, licenseTextFile);
+                File.WriteAllText(licenseTextFile, File.ReadAllText("LICENSE"));
+
+                Process.Start(new ProcessStartInfo(licenseTextFile));
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("There was a problem displaying the license details.", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                Logger.Log(LogLevel.Error, exception);
+            }
+
+            e.Handled = true;
         }
     }
 }
